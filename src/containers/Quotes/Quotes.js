1915e132ce44d60f3d79fb9d753e8cbbd0b51a0b
felipeLx <felipealvesfinanceiro@gmail.com>
2020-01-19 07:45:22 -0300

    refactor

--- src/containers/Quotes/Quotes.js
+++ src/containers/Quotes/Quotes.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 import QuoteItem from '../../components/Quotes/QuoteItem';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+// import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 // import Spinner from '../../components/UI/Spinner/Spinner';
 import Aux from '../../hoc/Aux/Aux';
 import axios from '../../axios-store';
@@ -10,61 +10,60 @@ import './Quotes.css';
 
 const url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
 
-const quotes = React.memo(props => {
-  const [data, setData] = useState({quotes: []});
+const Quotes = props => {
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [detailAllow, setDetailAllow] = useState(false);
+  const [isError,setIsError] = useState(false);
+  // const [detailAllow, setDetailAllow] = useState(false);
  
   useEffect(() => {
     setIsLoading(true);
-    console.log("isLoading: " + isLoading);
     const fetchData = async () => {
-      setIsError(false);
       setIsLoading(true);
       try {
         const result = await axios(url);
         setData(result.data);
-        console.log(result.data);
       } catch (error) {
         setIsError(true);
       }
       setIsLoading(false);
     };
     fetchData();
-  }, [isLoading]);
+  }, []);
 
-  
-    let shortDataItem = 
-    data.quotes.map((d) => 
-      <Aux onClick={() => setDetailAllow(true)}>
-        <QuoteItem
-          key={d.id}
-          id={d.id}
-          quote={d.content.rendered}
-          isFav={props.data.isFavorite}
-        />
-      </Aux>
+  let content = <p className="placeholder">Got no quotes yet!</p>;
+  if(data.length > 0) {
+    content = (
+      <ul className="quotes-list">
+        {data.map(d => (
+        <Aux key={d.id}>
+          <QuoteItem
+            key={d.id}
+            id={d.id}
+            quote={d.content.rendered}
+            isFav={props.isFavorite}
+          />
+        </Aux>
+        ))}
+      </ul>
     );
-
-    let bigDataItem = 
-      data.quotes.map((d) => 
-      <Aux onClick={() => setDetailAllow(false)}>
-        <QuoteItem
-          key={d.quotes.id}
-          id={d.quotes.id}
-          quote={d.quotes.content.rendered}
-          by={d.quotes.title.rendered}
-          isFav={props.data.isFavorite}
-        />
-      </Aux>
-      )
+  }  
+    
+    // const bigDataItem = (
+    //   data.quotes.map((d) => 
+    //   <Aux onClick={() => setDetailAllow(false)}>
+    //     <QuoteItem
+    //       key={d.quotes.id}
+    //       id={d.quotes.id}
+    //       quote={d.quotes.content.rendered}
+    //       by={d.quotes.title.rendered}
+    //       isFav={props.data.isFavorite}
+    //     />
+    //   </Aux>
+    //   )
+    // );
   
-  return (
-    <ul className="products-list">
-      {!detailAllow ? {shortDataItem} : {bigDataItem}}
-    </ul>
-  );
-});
+  return content;
+};
 
-export default (quotes);
+export default Quotes;
